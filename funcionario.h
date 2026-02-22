f50ab0957d75bf81ad62d5d30c3ced2df608c6da
Bruletario <brunoalmeidaacademico@gmail.com>
2026-02-22 14:34:15 -0300

    Enhance Funcionario class with new methods
    Added new methods for displaying details and calculating termination.

--- funcionario.h
+++ funcionario.h
@@ -13,7 +13,6 @@ protected:
     //informacoes sensiveis como protected
     std::string nome;
     std::string cpf;
-    std::string rg;
     Endereco endereco;
     
     // info p grade e organograma
@@ -23,6 +22,9 @@ protected:
     //controle interno, numero da contratacao
     static int contadorMatriculas; //conta o n de matriculas
     int matricula;                 //salva o n de matricula
+    
+    // Controle de status do funcionario na empresa
+    bool ativo; 
     
     //outras infos de contratacao, podemos adicionar mais coisas no futuro
     Data dataAdmissao;
@@ -35,7 +37,7 @@ protected:
 
 public:
     //construtor do funcionario
-    Funcionario(std::string nome, std::string cpf, std::string rg, Endereco end, Data admissao, double salario, TipoVinculo v,std::string turno = "Manha", std::string departamento = "Geral");
+    Funcionario(std::string nome, std::string cpf, Endereco end, Data admissao, double salario, TipoVinculo v,std::string turno = "Manha", std::string departamento = "Geral");
     
     //destrutor virtual
     virtual ~Funcionario() = default;
@@ -43,18 +45,30 @@ public:
     //Metodo que podem ser usados
     virtual double calcularSalario() const = 0; 
     virtual void render() const = 0;
+    
+    // Metodos virtuais para exibir dados completos e calcular rescisao 
+    virtual void exibirDetalhes() const = 0;
+    virtual double calcularRescisao(bool justaCausa) const = 0;
 
     //getters
     std::string getNome() const;
+    std::string getPrimeiroNome() const;
     int getMatricula() const;
     std::string getCpf() const;
     Data getDataAdmissao() const;
     double getSalarioBase() const;
-    std::string getRg() const;
     std::string getCargo() const;
     TipoVinculo getVinculo() const;
+    std::string getVinculoString() const;
     std::string getDepartamento() const; 
     std::string getTurno() const;
+    bool isAtivo() const; 
+
+    //setters para permitir a edicao de dados no gerenciamento
+    void setDepartamento(const std::string& d);
+    void setTurno(const std::string& t);
+    void setSalarioBase(double s);
+    void setAtivo(bool status);
 };
 
 #endif
